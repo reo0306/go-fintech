230ef2653fb2ef9ed0ba171366de3da9403ddd7b
reo0306 <reo19840306@gmail.com>
2026-05-17 08:48:55 +0900

    update selection4

--- section4.go
+++ section4.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"io"
+	"time"
+	"log"
 )
 
 func hy2(num int) string {
@@ -62,4 +66,160 @@ func main() {
 		fmt.Println(sum)
 	}
 	fmt.Println("final sum:", sum)
+
+	l := []string{"a", "b", "c"}
+
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
+	}
+
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
+
+	for _, v := range l {
+		fmt.Println(v)
+	}
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	for k := range m {
+		fmt.Println(k)
+	}
+
+	for _, v := range m {
+		fmt.Println(v)
+	}
+
+	switch os := getOsName(); os {
+	case "windows":
+		fmt.Println("Windows OS")
+	case "linux":
+		fmt.Println("Linux OS")
+	case "mac":
+		fmt.Println("Mac OS", os)
+	default:
+		fmt.Println("Unknown OS")
+	}
+
+	t := time.Now()
+	fmt.Println("Current time:", t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon!")
+	default:
+		fmt.Println("Good evening!")
+	}
+
+	/*defer fmt.Println("This will be printed last")
+
+	foo()
+
+	fmt.Println("This will be printed first")*/
+
+	/*
+	fmt.Println("This will be printed second")
+	defer fmt.Println("This will be printed last1")
+	defer fmt.Println("This will be printed last2")
+	defer fmt.Println("This will be printed last3")
+
+	fmt.Println("This will be printed first")
+	*/
+
+	/*file, _ := os.Open("./main.go")
+
+	defer file.Close()
+
+	data := make([]byte, 100)
+	file.Read(data)
+	fmt.Println(string(data))*/
+
+
+	/*LoggingSettings("./app.log")
+	_, err := os.Open("./test.go")
+	if err != nil {
+		log.Fatalln("Error opening file:", err)
+	}
+
+	log.Println("This is a log message")
+	log.Printf("Current time: %s", time.Now().Format(time.RFC3339))
+
+	log.Fatalf("%T %v", "error", "error!")
+	log.Fatalln("error!")*/
+
+	/*file, err := os.Open("./main.go")
+	if err != nil {
+		log.Fatalln("Error opening file:", err)
+	}
+	defer file.Close()
+
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error reading file:", err)
+	}
+	fmt.Println(count, string(data))
+
+	if err = os.Chdir("test"); err != nil {
+		log.Fatalln("Error changing directory:", err)
+	}*/
+
+	save()
+	fmt.Println("This will be printed after save()")
+
+	ll := []int{100, 300, 23, 11, 23, 2, 3, 6, 4}
+	for i := 0; i < len(ll); i++ {
+		if ll[i] == 2 {
+			fmt.Println(ll[i])
+		}
+	}
+
+	mm := map[string]int {
+		"apple": 200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi": 90,
+	}
+
+	sum2 := 0
+	for _, v := range mm {
+		sum2 += v
+	}
+	fmt.Println("Sum of values in map:", sum2)
+}
+
+func thirdPartyLib() {
+	panic("Unable to connect to database")
+}
+
+func save() {
+	defer func() {
+		s := recover()
+		fmt.Println("Recovered from panic:", s)
+	}()
+	thirdPartyLib()
+}
+
+func LoggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	mutiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(mutiLogFile)
+}
+
+func foo() {
+	defer fmt.Println("defer in foo")
+	fmt.Println("foo")
+}
+
+func getOsName() string {
+	return "mac"
 }
