ea99a26f43e2c3d87765e1b1b18ca6c96dc2d1ff
reo0306 <reo19840306@gmail.com>
2026-06-08 17:21:38 +0900

    update selection5

--- section5.go
+++ section5.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 )
 
+type Vertex struct {
+	X, Y int
+	S string
+}
+
 func one(x *int) {
 	*x = 1
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.X = 1000
 }
 
 func main() {
@@ -49,4 +62,38 @@ func main() {
 
 	var st = new(struct{})
 	fmt.Printf("%T\n", st)
+
+	v := Vertex{X: 1, Y: 2}
+	fmt.Println(v)
+	fmt.Println(v.X, v.Y)
+	v.X = 100
+	fmt.Println(v.X, v.Y)
+
+	v2 := Vertex{X: 1}
+	fmt.Println(v2)
+
+	v3 := Vertex{1, 2, "test"}
+	fmt.Println(v3)
+
+	v4 := Vertex{}
+	fmt.Printf("%T %v\n", v4, v4)
+
+	var v5 Vertex
+	fmt.Printf("%T %v\n", v5, v5)
+
+	v6 := new(Vertex)
+	fmt.Printf("%T %v\n", v6, v6)
+
+	v7 := &Vertex{}
+	fmt.Printf("%T %v\n", v7, v7)
+
+	v8 := Vertex{1, 2, "test"}
+	changeVertex(v8)
+	fmt.Println(v8)
+
+	v9 := &Vertex{1, 2, "test"}
+	changeVertex2(v9)
+	fmt.Println(*v9)
 }
+
+
