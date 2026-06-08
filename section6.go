6654225bd9648edd29751e98a377be281f24a4d8
reo0306 <reo19840306@gmail.com>
2026-06-08 18:00:18 +0900

    add selection6

---
+++ section6.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X, Y int
+}
+
+// 値れしバー
+func (v Vertex) Area() int {
+	return v.X * v.Y
+}
+
+//　ポインタれしバー
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
+func Area(v Vertex) int {
+	return v.X * v.Y
+}
+
+func main() {
+	v := Vertex{3, 4}
+	//fmt.Println(Area(v))
+	v.Scale(10)
+	fmt.Println(v.Area())
+}
