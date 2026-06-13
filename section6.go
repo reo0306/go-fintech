7b76549b5f3883acfb79ecc5dbe5d652b3e554d3
reo0306 <reo19840306@gmail.com>
2026-06-13 09:41:00 +0900

    update selection6

--- section6.go
+++ section6.go
@@ -5,27 +5,120 @@ import (
 )
 
 type Vertex struct {
-	X, Y int
+	x, y int
 }
 
-// 値れしバー
+// 値レシバー
 func (v Vertex) Area() int {
-	return v.X * v.Y
+	return v.x * v.y
 }
 
-//　ポインタれしバー
+//　ポインタレシバー
 func (v *Vertex) Scale(i int) {
-	v.X = v.X * i
-	v.Y = v.Y * i
+	v.x = v.x * i
+	v.y = v.y * i
 }
 
-func Area(v Vertex) int {
-	return v.X * v.Y
+//func Area(v Vertex) int {
+	//return v.x * v.y
+//}
+
+type Vertex3D struct {
+	Vertex
+	z int
+}
+
+// 値レシバー
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+//　ポインタレシバー
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
+}
+
+type MyInt int
+
+func (i MyInt) Double() int {
+	return int(i * 2)
+}
+
+type Human interface {
+	Say() string
+}
+
+type Person struct {
+	Name string
+}
+
+type Dog struct {
+	Name string
+}
+
+func (p *Person) Say() string {
+	p.Name = "Mr. " + p.Name
+	fmt.Printf("My name is %s\n", p.Name)
+	return p.Name
+}
+
+func DriveCar(human Human) {
+	if human.Say() == "Mr. Mike" {
+		fmt.Println("Driving car...")
+	} else {
+		fmt.Println("You can't drive a car.")
+	}
+}
+
+func do(i interface{}) {
+	/*ii := i.(int) // タイプアサーション
+	ii *= 2
+	fmt.Printf("The result is %v\n", ii)
+
+	ss := i.(string) // タイプアサーション
+	fmt.Printf("The result is %v\n", ss + " world")
+	*/
+	switch v := i.(type) { // switch type文
+	case int:
+		fmt.Printf("The result is %v\n", v * 2)
+	case string:
+		fmt.Printf("The result is %v\n", v + " world")
+	default:
+		fmt.Printf("Unknown type: %T\n", v)
+	}
 }
 
 func main() {
-	v := Vertex{3, 4}
+	//v := Vertex{3, 4}
+	v := New(3, 4, 5)
 	//fmt.Println(Area(v))
-	v.Scale(10)
-	fmt.Println(v.Area())
+	v.Scale3D(10)
+	//fmt.Println(v.Area())
+	fmt.Println(v.Area3D())
+
+	myInt := MyInt(10)
+	fmt.Println(myInt.Double())
+
+    var mike Human = &Person{"Mike"}
+    var x Human = &Person{"X"}
+    //var dog Dog = Dog{"Buddy"}
+	//mike.Say()
+	DriveCar(mike)
+	DriveCar(x)
+	//DriveCar(dog)
+
+	//var i interface{} = 10
+	do(10)
+	do("hello")
+	do(true)
+
+	var i int = 10
+	ii := float64(10)
+	fmt.Printf("The type of ii is %T\n", ii)
 }
