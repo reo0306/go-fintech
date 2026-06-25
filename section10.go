c283c39eeb25b0bb72f5fcf55cb8b1e3de93dd12
reo0306 <reo19840306@gmail.com>
2026-06-25 11:12:43 +0900

    update section10

--- section10.go
+++ section10.go
@@ -2,11 +2,44 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 )
+
+type T struct{}
+
+type Person struct {
+	Name      string   `json:"name"`
+	Age       int      `json:"age,omitempty"`
+	Nicknames []string `json:"nicknames,omitempty"`
+	T *T `json:"T,omitempty"`
+}
+
+func (p *Person) UnmarshalJSON(b []byte ) error {
+	type Person2 struct {
+		Name string
+	}
+	var p2 Person2
+	err := json.Unmarshal(b, &p2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	p.Name = p2.Name + "!"
+	return err
+}
+
+// Marshalのカスタマイズ
+/*func (p Person) MarshalJSON() ([]byte, error) {
+	v, err := json.Marshal(&struct {
+		Name string
+	}{
+		Name: "Mr." + p.Name,
+	})
+	return v, err
+}*/
 
 func main() {
 	/*resp, _ := http.Get("http://example.com")
@@ -33,4 +66,13 @@ func main() {
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(body))
 
+	b := []byte(`{"name":"Mike","age":"20","nicknames":["a","b","c"]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(p.Name, p.Age, p.Nicknames)
+
+	v, _ := json.Marshal(p)
+	fmt.Println(string(v))
 }
