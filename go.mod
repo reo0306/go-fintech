9a8b656f5609de24496e437ec14dc98529a863e9
reo0306 <reo19840306@gmail.com>
2026-06-26 19:08:29 +0900

    update section11

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.23.12
 require (
 	github.com/markcheno/go-quote v0.0.0-20251022180205-ebbbbdb8e2b0 // indirect
 	github.com/markcheno/go-talib v0.0.0-20250114000313-ec55a20c902f // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	gopkg.in/ini.v1 v1.67.3 // indirect
 )
--- section11.go
+++ section11.go
@@ -6,7 +6,16 @@ import (
 	"time"
 
 	"golang.org/x/sync/semaphore"
+	"gopkg.in/ini.v1"
 )
+
+type ConfigList struct {
+	Port int
+	DbName string
+	SQLDriver string
+}
+
+var Config ConfigList
 
 // go ルーチンで実行する数を制御できる
 var s *semaphore.Weighted = semaphore.NewWeighted(1)
@@ -26,6 +35,15 @@ func longProcess(ctx context.Context) {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Done")
 }
+
+func init() {
+	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList {
+		Port: cfg.Section("web").Key("port").MustInt(),
+		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+	}
+}
 func main() {
 	ctx := context.TODO()
 	go longProcess(ctx)
@@ -34,4 +52,8 @@ func main() {
 	time.Sleep(2 * time.Second)
 	go longProcess(ctx)
 	time.Sleep(5 * time.Second)
+
+	fmt.Printf("%T %v\n", Config.Port, Config.Port)
+	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
+	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
 }
