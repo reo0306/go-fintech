ea4ebadef35dd5088270fc1671d770826f768d46
reo0306 <reo19840306@gmail.com>
2026-06-27 11:10:16 +0900

    update section11

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-fintech
 go 1.23.12
 
 require (
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/markcheno/go-quote v0.0.0-20251022180205-ebbbbdb8e2b0 // indirect
 	github.com/markcheno/go-talib v0.0.0-20250114000313-ec55a20c902f // indirect
 	golang.org/x/sync v0.10.0 // indirect
--- section11.go
+++ section11.go
@@ -3,15 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/url"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"golang.org/x/sync/semaphore"
 	"gopkg.in/ini.v1"
+	//"github.com/markcheno/go-quote"
+	//"github.com/markcheno/go-talib"
 )
 
+type JsonRPC2 struct {
+	Version string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Result  interface{} `json:"result,omitempty"`
+	Id      *int        `json:"id,omitempty"`
+}
+
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
 type ConfigList struct {
-	Port int
-	DbName string
+	Port      int
+	DbName    string
 	SQLDriver string
 }
 
@@ -38,9 +55,9 @@ func longProcess(ctx context.Context) {
 
 func init() {
 	cfg, _ := ini.Load("config.ini")
-	Config = ConfigList {
-		Port: cfg.Section("web").Key("port").MustInt(),
-		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustInt(),
+		DbName:    cfg.Section("db").Key("name").MustString("example.sql"),
 		SQLDriver: cfg.Section("db").Key("driver").String(),
 	}
 }
@@ -56,4 +73,38 @@ func main() {
 	fmt.Printf("%T %v\n", Config.Port, Config.Port)
 	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
 	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
+
+	/*spy, _ := quote.NewQuoteFromYahoo(
+		"spy", "2026-05-01", "2026-06-01", quote.Daily, true)
+	fmt.Println(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
+	mva := talib.Ema(spy.Close, 14)
+	fmt.Println(mva)*/
+
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
+		log.Fatal("subscribe:", err)
+		return
+	}
+
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Println("read:", err)
+			return
+		}
+
+		if message.Method == "channelMessage" {
+			log.Println(message.Params)
+		}
+	}
 }
